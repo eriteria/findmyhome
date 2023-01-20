58f6ee360d968cc1f450cf395a7b2a013b7d1727
eriteria <joseph.john.ali.2001@gmail.com>
2023-01-20 16:26:32 +0100

    Compulsory update

--- findmyhome/main/views.py
+++ findmyhome/main/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello World")
+    return render(request, "main/home.html", {"message": "Hello World"})
